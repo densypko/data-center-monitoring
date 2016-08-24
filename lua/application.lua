8019c6f770eb3fb9d189eb1ada58edadb296c91f
Carlos Ballesteros <caballes@ucm.es>
2016-08-24 17:32:52 +0200

    Envía la temperatura por MQTT y HTTP cada 60 segundos y entra en sueño profundo

--- lua/application.lua
+++ lua/application.lua
@@ -1,16 +1,26 @@
 local module = {}  
+
 m = nil
+t = nil
 WRITEKEY="ZXKQ6RBIYQA6DZWG" -- Tu API Key de thingspeak.com
 working_count = 0
 
---- INTERVAL ---
--- In milliseconds. Remember that the sensor reading, 
--- reboot and wifi reconnect takes a few seconds
-time_between_sensor_readings = 60000                               
 
--- Sends a simple ping to the broker
+--- INTERVAL ---
+-- In milliseconds, (x * 60000) where x = minutes . Remember that the sensor reading, 
+-- reboot and wifi reconnect takes a few seconds
+time_between_sensor_readings = 1*60000                               
+
+-- All topics:  nodemcu/# 
+
+-- Topic: nodemcu/ping Sends a simple ping to the broker
 local function send_ping()  
     m:publish(config.ENDPOINT .. "ping","id=" .. config.ID,0,0)
+end
+
+-- Topic: nodemcu/temperature Sends a simple temperature to the broker
+local function send_tmp_mqtt()
+    m:publish(config.ENDPOINT .. "temperature","t=" .. t,0,0)
 end
 
 -- Sends my id to the broker for registration
@@ -21,8 +31,8 @@ local function register_myself()
 end
 
 local function mqtt_start()  
+    
     m = mqtt.Client(config.ID, 120)
-  
     -- register message callback beforehand
     m:on("message", function(conn, topic, data) 
       if data ~= nil then
@@ -34,10 +44,11 @@ local function mqtt_start()
   
     -- Connect to broker
     m:connect(config.HOST, config.PORT, 0, 1, function(con) 
-        register_myself()
+        --register_myself()
         -- And then pings each 1000 milliseconds
-        tmr.stop(6)
-        tmr.alarm(6, 1000, 1, send_ping)
+        --tmr.stop(6)
+        send_tmp_mqtt()
+        send_ping() 
     end) 
 
 end
@@ -64,12 +75,19 @@ local function ds18b20_start()
                 print("Going to deep sleep "..(time_between_sensor_readings/1000).." seconds")
                 node.dsleep(time_between_sensor_readings*1000)             
             else
-            print("Total DS18B20 sensors: "..table.getn(addrs))
-            -- Leemos la temperatura dos veces por si hay algun error
-            t = ds18b20.read()
-            t = ds18b20.read()
-            print("Temperature: "..t.."'C\n")
-            sendData(t)
+                print("Total DS18B20 sensors: "..table.getn(addrs))
+                -- Leemos la temperatura dos veces por si hay algun error
+                t = ds18b20.read()
+                t = ds18b20.read()
+                print("Temperature: "..t.."'C\n")
+                
+                -- La envíamos por mqtt
+                mqtt_start()
+            
+                -- La envíamos por tcp
+                sendData(t)
+            
+             
             end
         end 
     end
@@ -88,23 +106,27 @@ end
 
 -- Enviar datos a https://api.thingspeak.com
 function sendData(temp)
-    conn = nil
-    conn = net.createConnection(net.TCP, 0)
-    conn:on("receive", function(conn, payload)success = true print(payload)end)
-    conn:on("connection",
+    con = nil
+    con = net.createConnection(net.TCP, 0)
+    con:on("receive", function(conn, payload)success = true print(payload)end)
+    con:on("connection",
     function(conn, payload)
     print("Connectado")
-    conn:send('GET /update?key='..WRITEKEY..'&field1='..temp..'HTTP/1.1\r\n\Host: api.thingspeak.com\r\nAccept: */*\r\nUser-Agent: Mozilla/4.0 (compatible; esp8266 Lua; Windows NT 5.1)\r\n\r\n')end)
-    conn:on("disconnection", function(conn, payload) print('Desconectado') end)
-    conn:connect(80,'184.106.153.149')
+    con:send('GET /update?key='..WRITEKEY..'&field1='..temp..'HTTP/1.1\r\n\Host: api.thingspeak.com\r\nAccept: */*\r\nUser-Agent: Mozilla/4.0 (compatible; esp8266 Lua; Windows NT 5.1)\r\n\r\n')end)
+    con:on("disconnection", function(conn, payload) print('Desconectado') end)
+    con:connect(80,'184.106.153.149')
 end
 
 function module.start()  
 
+  
   ds18b20_start()
+  
   -- send data every 10000 ms to thing speak
-  tmr.alarm(1,10000, 1, function() ds18b20_start() end)
-  --mqtt_start()
+  tmr.alarm(1,20000, 1, function() ds18b20_start() end)
+  
+  
+  
 end
 
 return module  
